c68ccfb03bc2082db5589675e8c38209801c8c2c
Manu_i_003 <manikantathota2003@gmail.com>
2026-04-07 18:42:55 +0530

    changes done in config file

--- src/config/config.py
+++ src/config/config.py
@@ -10,8 +10,6 @@ Central place to store:
 - Constants
 - Parameters
 
-This avoids hardcoding values across files.
-
 =========================================================
 """
 
@@ -21,19 +19,31 @@ This avoids hardcoding values across files.
 TRAIN_DATA_PATH = "data/raw/Train.csv"
 TEST_DATA_PATH = "data/raw/Test.csv"
 
+# ===============================
+# Processed Data Paths
+# ===============================
+PROCESSED_DATA_DIR = "data/processed/"
+
+AGGREGATED_DATA_PATH = PROCESSED_DATA_DIR + "aggregated_encoded.csv"
+TIME_SERIES_DATA_PATH = PROCESSED_DATA_DIR + "time_series.csv"
+
+X_TRAIN_PATH = PROCESSED_DATA_DIR + "X_train.csv"
+X_TEST_PATH = PROCESSED_DATA_DIR + "X_test.csv"
+Y_TRAIN_PATH = PROCESSED_DATA_DIR + "y_train.csv"
+Y_TEST_PATH = PROCESSED_DATA_DIR + "y_test.csv"
+
+TS_TRAIN_PATH = PROCESSED_DATA_DIR + "train_ts.csv"
+TS_TEST_PATH = PROCESSED_DATA_DIR + "test_ts.csv"
 
 # ===============================
-# Output Paths
+# Model Path
 # ===============================
-PROCESSED_DATA_PATH = "data/processed/"
 MODEL_SAVE_PATH = "models/xgboost_model.pkl"
-
 
 # ===============================
 # Split Configuration
 # ===============================
 TEST_SIZE_RATIO = 0.2
-
 
 # ===============================
 # Random Seed
